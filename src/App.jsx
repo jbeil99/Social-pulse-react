7ca55f32aef192f7622878ea8be50967a5a5ed68
MahmoudEissaM <mahmoudessa8200@gmail.com>
2025-04-22 23:25:25 +0200

    Save my current changes before restoring old files

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,8 @@ import './App.css'
 import YouTubeShortCreator from './components/forms/YouTubeShortCreator'
 import NavbarComponent from "./components/navbar/NavbarComponent";
 import SideMenuComponent from "./components/sidemenu/SideMenuComponent";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NewButton from './components/navbar/NewButton';
-=======
-import PostForm from './components/forms/PostForm'
-import BufferPostUI from './components/forms/postui'
->>>>>>> 30e2dc6 (remodify)
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,22 +15,13 @@ function App() {
     <>
       <NavbarComponent />
       <SideMenuComponent />
-<<<<<<< HEAD
       {/* <YouTubeShortCreator /> */}
       <Router>
-      <Routes>
-        <Route path="/" element={<NewButton />} />
-        {/* <Route path="/youtube-  short-creator" element={<YouTubeShortCreator />} /> */}
-      </Routes>
-    </Router>
-=======
-      <YouTubeShortCreator />
-      {/* <BufferPostUI /> */}
-      {/* <div className="min-h-screen bg-gray-100 p-4">
-      <PostForm/>
-      
-    </div> */}
->>>>>>> 30e2dc6 (remodify)
+        <Routes>
+          <Route path="/" element={<NewButton />} />
+          {/* <Route path="/youtube-short-creator" element={<YouTubeShortCreator />} /> */}
+        </Routes>
+      </Router>
     </>
     
   )
